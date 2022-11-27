e24aa154d7f49a4d081704ec2892ef73e886a782
Johnny Bieniek <jjbieniek@gmail.com>
2022-11-26 19:48:57 -0500

    initial commit, the base setup seems to be working

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+import { MoralisProvider } from "react-moralis"
+import { NotificationProvider } from "web3uikit"
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <div>
+            <Head>
+                <title>Encode Lottery</title>
+                <meta name="description" content="Decentralized lottery" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <NotificationProvider>
+                    <Component {...pageProps} />
+                </NotificationProvider>
+            </MoralisProvider>
+        </div>
+    )
 }
 
 export default MyApp
